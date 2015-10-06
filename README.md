ed36044cecfc1795792c9a58afb6a9e6f0ae17da
adm3923 <ash.austin@bendigoadelaide.com.au>
2015-10-06 14:29:22 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Cloud Foundry Meteor Buildpack
+==============================
+
+##Why this Buildpack?
+
+This buildpack was modified and tested on _IBM Bluemix_. It uses the latest meteor available at https://install.meteor.com/
+
+##Run apps easily with 'meteor' using MongoDB and/or Cloudant NoSQL 
+
+No need to specify ENV_VARIABLES before Deployment or before runtime, just ensure the services are bound to your application at runtime.
+
+##What variables does this specify?
+
+There are 3 variables that are gathered from VCAP_* prior to runtime.
+There are 2 variables that are set if constraints are met
+
+###ROOT_URL
+
+Required by meteor apps to start. Parsed from VCAP_APPLICATION prior to runtime. If you are using an alternate ROOT_URL instead of the appname.domain standard, specify this manually on your application or in your manifest.yml to override
+
+###MONGO_URL
+
+As with ROOT_URL this is parsed prior to runtime from VCAP_SERVICES, if you wish to override this option, specify the value in your manifest.yml or manually against your application.
+
+###COUCHDB_URL
+
+As with MONGO_URL this is parsed prior to runtime from VCAP_SERVICES, if you wish to override this option, specify the value in your manifest.yml or manually against your application.
+
+###KADIRA_APP_* (meteorhacks:kadira)
+
+KADIRA_APP_ID and KADIRA_APP_SECRET are parsed from ./kadira.settings if the file exists and "meteorhacks:kadira" package is installed. This is the case unless they are explicitly defined on the application or in the manifest
+
+###CLUSTER_WORKERS_COUNT (meteorhacks:cluster)
+
+CLUSTER_WORKERS_COUNT=auto is set if the "meteorhacks:cluster" package is installed. This is the case unless they are explicitly defined on the application or in the manifest.
---
+++ profile/env.sh
@@ -0,0 +1,51 @@
+jq=$HOME/.vendor/jq
+
+extract_mongo_url() {
+  # Set if not already present.
+  if [ -z "${MONGO_URL}" ]; then
+    #echo "Pulling MONGO_URL from VCAP_SERVICES"
+    # Find the first service key that contains "mongo", then grab the 'url' (or 'uri') key beneath it.
+    export MONGO_URL=`echo $VCAP_SERVICES | $jq 'to_entries|map(select(.key|contains("mongo")))[0]|.value[0].credentials|if .url then .url else .uri end|. // empty' | sed -e 's/^"//'  -e 's/"$//'`
+  fi
+  #echo "MONGO_URL: $MONGO_URL"
+}
+
+extract_root_url() {
+  if [ -z "${ROOT_URL}" ]; then
+    #echo "Pulling ROOT_URL from VCAP_APPLICATION"
+    DOMAIN=`echo $VCAP_APPLICATION | $jq '.uris[0]' | sed -e 's/^"//'  -e 's/"$//'`
+    export ROOT_URL="http://$DOMAIN"
+    #echo "ROOT_URL: $ROOT_URL"
+  fi
+}
+
+extract_couchdb_url() {
+  if [ -z "${COUCHDB_URL}" ]; then
+    #echo "Pulling COUCHDB_URL from VCAP_SERVICES"
+    # Find the first service key that contains "cloudant", then grab the 'url' (or 'uri') key beneath it.
+    export COUCHDB_URL=`echo $VCAP_SERVICES | $jq 'to_entries|map(select(.key|contains("cloudant")))[0]|.value[0].credentials|if .url then .url else .uri end|. // empty'  | sed -e 's/^"//'  -e 's/"$//'`
+  fi
+}
+
+discover_cluster() {
+    if [ -d "/app/.meteor/local/plugin-cache/meteorhacks_cluster" ] && [ -z "${CLUSTER_WORKERS_COUNT}" ]; then
+     export CLUSTER_WORKERS_COUNT=auto
+    else
+    echo "No meteorhacks:cluster config required"
+    fi
+}
+
+discover_kadira() {
+    if [ -f "/app/kadira.settings" ] && [ -d "/app/.meteor/local/plugin-cache/meteorhacks_kadira" ] && [ -z "${KADIRA_APP_ID}" ]; then
+      export KADIRA_APP_ID=`awk '/kadira_app_id/{ print $0 }' /app/kadira.settings | awk  -F"=" '{print $2}'`
+      export KADIRA_APP_SECRET=`awk '/kadira_app_secret/{ print $0 }' /app/kadira.settings | awk  -F"=" '{print $2}'`
+    else
+    echo "No automatic meteorhacks:kadira config required"
+    fi
+}
+
+extract_mongo_url
+extract_root_url
+extract_couchdb_url
+discover_cluster
+discover_kadira
